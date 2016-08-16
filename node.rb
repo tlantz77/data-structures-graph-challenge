aec291007b2423a99d041f25d0db43e109dba9bf
tlantz77 <tlantz@gmail.com>
2016-08-16 14:56:19 -0500

    implement node class and tests

--- node.rb
+++ node.rb
@@ -1,6 +1,22 @@
 class Node
 
-  def initialize
+  attr_accessor :value, :edges
+
+  def initialize(value)
+    @value = value
+    @edges = []
   end
+
+  def add_edge(other_node)
+    @edges << other_node
+  end
+
+  def nodes
+    @edges.each do |edge|
+      p edge.value
+    end
+  end
+
+
 
 end
--- node_spec.rb
+++ node_spec.rb
@@ -2,4 +2,36 @@ require_relative 'node'
 
 describe Node do
 
+  let(:alyssa) { Node.new("Alyssa") }
+  let(:casey) { Node.new("Casey") }
+  let(:duke) { Node.new("Duke") }
+  let(:jones) { Node.new("Jones") }
+  let(:matt) { Node.new("Matt") }
+  let(:maurice) { Node.new("Maurice") }
+  let(:mike) { Node.new("Mike") }
+
+  before(:each) do
+    matt.edges = [mike, maurice]
+    mike.edges = [casey, duke, maurice]
+    duke.edges = [alyssa]
+    jones.edges = [casey]
+  end
+
+  it "can return its value" do
+    expect(duke.value).to eq("Duke")
+  end
+
+  it "can add other nodes as edges" do
+    matt.add_edge(casey)
+    expect(matt.edges).to include(mike)
+    expect(matt.edges).to include(casey)
+    expect(matt.edges).not_to include(alyssa)
+  end
+
+  it "can output the values of its edges" do
+    expect(mike.nodes).to eq( [ casey, duke, maurice ] )
+    expect  { mike.nodes }.to output("\"Casey\"\n\"Duke\"\n\"Maurice\"\n").to_stdout
+  end
+
+
 end
